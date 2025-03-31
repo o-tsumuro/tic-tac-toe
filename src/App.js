e3057fbaa689de2351c3cf91723f124602e488ac
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-03-31 22:33:26 +0900

    盤面作成

--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
 export default function Square() {
-  return <button className="square">X</button>;
+  return (
+    <>
+      <div className="board-row">
+        <div className="square">1</div>
+        <div className="square">2</div>
+        <div className="square">3</div>
+      </div>
+      <div className="board-row">
+        <div className="square">4</div>
+        <div className="square">5</div>
+        <div className="square">6</div>
+      </div>
+      <div className="board-row">
+        <div className="square">7</div>
+        <div className="square">8</div>
+        <div className="square">9</div>
+      </div>
+    </>
+  );
 }
