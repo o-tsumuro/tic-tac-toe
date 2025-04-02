252e85b23e60c5d59489c50807a351254c36ea3d
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-04-02 22:21:01 +0900

    早期リターン追加

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 
 function Square({ value, onSquareClick }) {
-  return <button className="square" onClick={onSquareClick}>
-    {value}
-  </button>
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
@@ -11,6 +13,9 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
